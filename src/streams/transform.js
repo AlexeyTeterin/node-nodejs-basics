fdb0cb719790a2b7ef243ee70b893a2385dff96f
Alexey Teterin <ateterin@naumen.ru>
2022-06-05 18:45:13 +0500

    feat: streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
-const transform = async () => {
-    // Write your code here 
+import {stdin, stdout} from 'process';
+import {Transform, pipeline} from 'stream';
+
+const reverseTransform = (chunk, encoding, callback) => {
+  callback(null, chunk.reverse() + '\n\n');
+};
+
+const reverseStream = new Transform({transform: reverseTransform});
+
+export const transform = async () => {
+  pipeline(stdin, reverseStream, stdout, () => {});
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
-const write = async () => {
-    // Write your code here 
+import {stdin} from 'process';
+import {createWriteStream} from 'fs';
+import path from 'path';
+
+export const write = async () => {
+  const filePath = path.resolve('files', 'fileToWrite.txt');
+  const stream = createWriteStream(filePath);
+
+  stdin.pipe(stream);
+  stream.on('error', (err) => console.log(err.message));
 };
+
 
 await write();
