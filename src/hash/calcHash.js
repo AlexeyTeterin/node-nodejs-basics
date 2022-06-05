16588cabceb8d506bb81ad101c627da007deac57
Alexey Teterin <ateterin@naumen.ru>
2022-06-05 15:02:03 +0500

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
-const calculateHash = async () => {
-    // Write your code here 
+import {createHash} from 'crypto';
+import path from 'path';
+import {readFile} from 'fs/promises';
+
+export const calculateHash = async () => {
+  const filename = path.resolve('files', 'fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+
+  hash.update(await readFile(filename));
+
+  const hex = hash.digest('hex');
+  console.log('hex:', hex);
+
+  return hex;
 };
 
-await calculateHash();
+calculateHash();
