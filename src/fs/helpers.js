60406e11457d9bc7a1de36b2929be6ab179e7af4
Alexey Teterin <ateterin@naumen.ru>
2022-11-29 22:19:47 +0500

    fix: path

--- src/fs/helpers.js
+++ src/fs/helpers.js
@@ -1,7 +1,7 @@
 import path from 'path';
 
-export const DIR_FILES = path.resolve('files');
-export const DIR_FILES_COPY = path.resolve('files_copy');
+export const DIR_FILES = path.join('src', 'fs', 'files');
+export const DIR_FILES_COPY = path.join('src', 'fs', 'files_copy');
 export const FILE_FRESH = 'fresh.txt';
 
 
